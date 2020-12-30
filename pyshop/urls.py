887c0042f297cb3859a4c6fb0b717144ceab55b1
oduorsamuel <samuel@techfactory.ltd>
2020-12-30 13:07:36 +0300

    Updated homepage url

--- pyshop/urls.py
+++ pyshop/urls.py
@@ -19,7 +19,7 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('product/', include('products.urls')),
+    path('', include('products.urls')),
     path('users/', include('users.urls'))
 ]
 urlpatterns += staticfiles_urlpatterns()
