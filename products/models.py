8642120eca68c19d69e3cecf6c46b9f303467325
oduorsamuel <samuel@techfactory.ltd>
2021-01-07 14:55:58 +0300

    DB set_Up

--- products/models.py
+++ products/models.py
@@ -4,12 +4,38 @@ from django.db import models
 # Create your models here.
 class Products(models.Model):
     name = models.CharField(max_length=255)
-    price = models.FloatField()
-    stock = models.IntegerField()
-    image_url = models.CharField(max_length=2083)
+    details = models.CharField(max_length=80000)
+    price = models.CharField(max_length=255)
+    old_price = models.CharField(max_length=250)
+    file = models.CharField(max_length=250)
+    category_id = models.IntegerField()
+    category_name = models.CharField(max_length=250)
+    sub_category_id = models.IntegerField()
+    sub_category_name = models.CharField(max_length=250)
+    brand_id = models.IntegerField()
+    brand_name = models.CharField(max_length=250)
+    is_deleted = models.BooleanField(default=False, editable=True)
+    created = models.DateTimeField(auto_now_add=True)
 
 
-class Offer(models.Model):
-    code = models.CharField(max_length=255)
-    description = models.CharField(max_length=255)
-    discount = models.FloatField()
+class Brand(models.Model):
+    name = models.CharField(max_length=255)
+    category_id = models.IntegerField()
+    category_name = models.CharField(max_length=250)
+    sub_category_id = models.IntegerField()
+    sub_category_name = models.CharField(max_length=250)
+    is_deleted = models.BooleanField(default=False, editable=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Category(models.Model):
+        name = models.CharField(max_length=255)
+        is_deleted = models.BooleanField(default=False, editable=True)
+        created = models.DateTimeField(auto_now_add=True)
+
+    class SubCategory(models.Model):
+        name = models.CharField(max_length=255)
+        category_id = models.IntegerField()
+        category_name = models.CharField(max_length=255)
+        is_deleted = models.BooleanField(default=False, editable=True)
+        created = models.DateTimeField(auto_now_add=True)
+
--- pyshop/settings.py
+++ pyshop/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
@@ -26,7 +25,6 @@ SECRET_KEY = '3*&&4b9)e0j@$*=i6k68*9)bg%1ns&4rlr_6ber&kucyyzn4!e'
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -73,17 +71,19 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'pyshop.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'maajeega',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '3306'
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
@@ -103,7 +103,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
@@ -116,7 +115,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
