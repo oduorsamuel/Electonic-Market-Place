4f5d5acdc6b6d52fa3a1718f76c9d9ed0f0b2434
oduorsamuel <samuel@techfactory.ltd>
2021-01-08 15:03:23 +0300

    Registered models

--- products/admin.py
+++ products/admin.py
@@ -1,4 +1,27 @@
 from django.contrib import admin
-from .models import Products
+from .models import Products, Brand, Category, SubCategory
 
 # Register your models here.
+admin.site.site_header = 'Dashboard'
+
+
+class ProductsAdmin(admin.ModelAdmin):
+    fields = ('name', 'price', 'category')
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'created')
+
+
+class SubCategoryAdmin(admin.ModelAdmin):
+    fields = ('name', 'category_name')
+
+
+class BrandAdmin(admin.ModelAdmin):
+    fields = ('name', 'category_name', 'sub_category_name')
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(SubCategory, SubCategoryAdmin)
+admin.site.register(Brand, BrandAdmin)
+admin.site.register(Products, ProductsAdmin)
--- products/models.py
+++ products/models.py
@@ -2,40 +2,42 @@ from django.db import models
 
 
 # Create your models here.
-class Products(models.Model):
+
+class Category(models.Model):
     name = models.CharField(max_length=255)
-    details = models.CharField(max_length=80000)
-    price = models.CharField(max_length=255)
-    old_price = models.CharField(max_length=250)
-    file = models.CharField(max_length=250)
-    category_id = models.IntegerField()
-    category_name = models.CharField(max_length=250)
-    sub_category_id = models.IntegerField()
-    sub_category_name = models.CharField(max_length=250)
-    brand_id = models.IntegerField()
-    brand_name = models.CharField(max_length=250)
+    is_deleted = models.BooleanField(default=False, editable=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+
+class SubCategory(models.Model):
+    name = models.CharField(max_length=255)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    category_name = models.CharField(max_length=255)
     is_deleted = models.BooleanField(default=False, editable=True)
     created = models.DateTimeField(auto_now_add=True)
 
 
 class Brand(models.Model):
     name = models.CharField(max_length=255)
-    category_id = models.IntegerField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
     category_name = models.CharField(max_length=250)
-    sub_category_id = models.IntegerField()
+    sub_category = models.ForeignKey(SubCategory, on_delete=models.CASCADE)
     sub_category_name = models.CharField(max_length=250)
     is_deleted = models.BooleanField(default=False, editable=True)
     created = models.DateTimeField(auto_now_add=True)
 
-    class Category(models.Model):
-        name = models.CharField(max_length=255)
-        is_deleted = models.BooleanField(default=False, editable=True)
-        created = models.DateTimeField(auto_now_add=True)
 
-    class SubCategory(models.Model):
-        name = models.CharField(max_length=255)
-        category_id = models.IntegerField()
-        category_name = models.CharField(max_length=255)
-        is_deleted = models.BooleanField(default=False, editable=True)
-        created = models.DateTimeField(auto_now_add=True)
-
+class Products(models.Model):
+    name = models.CharField(max_length=255)
+    details = models.CharField(max_length=80000)
+    price = models.CharField(max_length=255)
+    old_price = models.CharField(max_length=250)
+    file = models.CharField(max_length=250)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    category_name = models.CharField(max_length=250)
+    sub_category = models.ForeignKey(SubCategory, on_delete=models.CASCADE)
+    sub_category_name = models.CharField(max_length=250)
+    brand = models.ForeignKey(Brand, on_delete=models.CASCADE)
+    brand_name = models.CharField(max_length=250)
+    is_deleted = models.BooleanField(default=False, editable=True)
+    created = models.DateTimeField(auto_now_add=True)
