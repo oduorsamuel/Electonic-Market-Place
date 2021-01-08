918e8ffb5dd1ee573e401a0bd9ce4d6f6fa4cfc8
oduorsamuel <samuel@techfactory.ltd>
2021-01-08 08:47:26 +0300

    settings.py: mysqlclient set-up

--- products/admin.py
+++ products/admin.py
@@ -1,16 +1,4 @@
 from django.contrib import admin
-from .models import Products, Offer
-
-
-class ProductAdmin(admin.ModelAdmin):
-    list_display = ('name', 'price', 'stock')
-
-
-class OfferAdmin(admin.ModelAdmin):
-    list_display = ('code', 'description', 'discount')
-
-
-admin.site.register(Products, ProductAdmin)
-admin.site.register(Offer, OfferAdmin)
+from .models import Products
 
 # Register your models here.
